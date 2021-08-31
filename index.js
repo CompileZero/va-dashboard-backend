2320c1bad9f7030d1f5694a36fa56cef376c5309
Atharva Kulkarni <atharvaak66@gmail.com>
2021-08-31 10:13:30 +0100

    updated test yaml

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    // res.send("To access the Netherlands data, proceed add /nldata to your URL");
+
     res.send(datafile);
 });
 
